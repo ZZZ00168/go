457d4e3eba85485b9b548e7e4d59d28f396b0cdc
ZZZ00168 <15521085815@163.com>
2021-11-17 15:38:20 +0800

    图片展示

---
+++ img_show/dbtest.go
@@ -0,0 +1,21 @@
+package main
+ 
+import (
+	"fmt"
+	"log"
+	"net/http"
+ 
+	"github.com/gin-gonic/gin"
+)
+ 
+func Index(c *gin.Context) {
+	c.HTML(http.StatusOK, "img_show.html", gin.H{})
+}
+
+func main() {
+	r := gin.Default()
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	r.LoadHTMLGlob("img_show.html")
+	r.GET("/", Index)
+	r.Run(":9090")
+}
