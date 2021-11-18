c9000337d9acab5b5fac536b15b103f8973388d4
ZZZ00168 <15521085815@163.com>
2021-11-18 22:24:21 +0800

    .

--- get_info2/analyse.go
+++ get_info2/analyse.go
@@ -15,22 +15,22 @@ func parseUrls(url string) {
 	bodystr := mahonia.NewDecoder("utf-8").ConvertString(string(body))
 	fmt.Println(bodystr)
 	
-	urls := regexp.MustCompile(`(I\((.*)"\))`)
+	urls := regexp.MustCompile(`(I\("(.*)")`)
 	items := urls.FindAllStringSubmatch(body, -1)
 	for _, item := range items {
-		fmt.Println(item[0])
+		fmt.Println(item[2])
 	}
 	
-	
+	for _, item := range items {
+		fmt.Println(decode(item[2]))
+	}
 
 }
 
 func decode(s string) string{
 	n := ""
-	
 	for _, c := range s{
 		n += string(128^c);
 	}
-	
 	return n
 }
